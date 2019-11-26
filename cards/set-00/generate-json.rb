d828d1519c0889ebefc6b05dd4f56c789f52d81d
Zoey Child <imperialoctopus@gmail.com>
2019-11-26 13:22:10 +0000

    Add json output

--- cards/set-00/generate-json.rb
+++ cards/set-00/generate-json.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'json'
 
-options = ARGV
-
-if options.length != 2
-  raise 'Usage: ruby generate-json.rb [csv] [output folder]'
+if ARGV.length != 2
+  puts 'Usage: ruby generate-json.rb [csv] [output folder]'
+  exit(1)
 end
 
-puts CSV.read(options[0], headers: true)
+lines = CSV.open(ARGV[0]).readlines
+keys = lines.delete lines.first
+
+File.open(ARGV[1], 'w') do |f|
+  data = lines.map do |values|
+    Hash[keys.zip(values)]
+  end
+  f.puts JSON.pretty_generate(data)
+end
 
 # File.open(options[0]).each_line do |line|
 #   puts line
