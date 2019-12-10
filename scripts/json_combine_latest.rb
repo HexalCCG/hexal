dfacfcf94bdbe0fb772bb17089add153ece2dc99
Zoey Child <imperialoctopus@gmail.com>
2019-12-10 14:10:47 +0000

    Add json_combine_latest script

--- scripts/json_combine_latest.rb
+++ scripts/json_combine_latest.rb
@@ -1,27 +1,29 @@
-require 'csv'
+require 'optparse'
 require 'json'
 
-element_keys = {
-  'earth' => 'e',
-  'fire' => 'f',
-  'air' => 'a',
-  'water' => 'w',
-  'spirit' => 's',
-  'any' => 'r'
-}
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: json_combine_latest.rb [options]"
 
-if ARGV.length != 2
-  puts 'Usage: ruby json_combine_latest.rb [input folder] [output.json]'
-  exit(1)
+  opts.on("-i", "--input=INPUT", String, "Input folder") do |v|
+    options[:input] = v
+  end
+  opts.on("-o", "--output=OUTPUT", String, "Output file") do |v|
+    options[:output] = v
+  end
+end.parse!
+
+if options[:input].nil? 
+  raise OptionParser::MissingArgument
+end
+if options[:output].nil?
+  options[:output] = "latest.json"
 end
 
-if File.file?(ARGV[0] + "/manifest.json")
-  meta_hash = JSON.parse(File.read(ARGV[0] + "/manifest.json"))
-elsif 
-  meta_hash = {}
-end
+ignore_files = ["/manifest.json", "/latest.json"]
+ignore_files_reg = Regexp.union(ignore_files)
 
-cards_hash = Dir[ARGV[0] + '/*.json'].delete_if {|name| name.match?("/manifest.json$")}.to_h do |location|
+cards_hash = Dir[options[:input] + '/*.json'].delete_if {|name| name.match?(ignore_files_reg)}.to_h do |location|
   file = JSON.parse(File.read(location))
 
   latest = file.max { |(ak, _), (bk, _)| ak.to_i <=> bk.to_i }[1]
@@ -29,11 +31,20 @@ cards_hash = Dir[ARGV[0] + '/*.json'].delete_if {|name| name.match?("/manifest.j
   [latest['id'], latest]
 end
 
+if File.file?(options[:input] + "/manifest.json")
+  meta_hash = JSON.parse(File.read(options[:input] + "/manifest.json"))
+else
+  meta_hash = {}
+end
+
+meta_hash["length"] = cards_hash.length
+meta_hash["version"] = cards_hash.inject(0) { |sum, n| sum + n[1]["version"] } - cards_hash.length
+
 hash = {
   "meta" => meta_hash,
   "cards" => cards_hash
 }
 
-File.open(ARGV[1], 'w') do |file|
+File.open(options[:output], 'w') do |file|
   file.puts(JSON.pretty_generate(hash))
 end
