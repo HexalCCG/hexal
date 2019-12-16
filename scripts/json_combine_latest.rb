f7f2ad4e14463ce0143a343ffefd4e0f063525df
Zoey Child <imperialoctopus@gmail.com>
2019-12-16 15:58:05 +0000

    Flatten meta section of json output

--- scripts/json_combine_latest.rb
+++ scripts/json_combine_latest.rb
@@ -23,27 +23,22 @@ end
 ignore_files = ["/manifest.json", "/latest.json"]
 ignore_files_reg = Regexp.union(ignore_files)
 
-cards_array = Dir[options[:input] + '/*.json'].delete_if {|name| name.match?(ignore_files_reg)}.map do |location|
+cards = Dir[options[:input] + '/*.json'].delete_if {|name| name.match?(ignore_files_reg)}.map do |location|
   file = JSON.parse(File.read(location))
   latest = file.max { |(ak, _), (bk, _)| ak.to_i <=> bk.to_i }[1]
-
   latest
 end
 
 if File.file?(options[:input] + "/manifest.json")
-  meta_hash = JSON.parse(File.read(options[:input] + "/manifest.json"))
+  set = JSON.parse(File.read(options[:input] + "/manifest.json"))
 else
-  meta_hash = {}
+  set = {}
 end
 
-meta_hash["length"] = cards_array.length
-meta_hash["version"] = cards_array.inject(0) { |sum, n| sum + n["version"] } - cards_array.length
-
-hash = {
-  "meta" => meta_hash,
-  "cards" => cards_array
-}
+set["length"] = cards.length
+set["version"] = cards.inject(0) { |sum, n| sum + n["version"] } - cards.length
+set["cards"] = cards
 
 File.open(options[:output], 'w') do |file|
-  file.puts(JSON.pretty_generate(hash))
+  file.puts(JSON.pretty_generate(set))
 end
